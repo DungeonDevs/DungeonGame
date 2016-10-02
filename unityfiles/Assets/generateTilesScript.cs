f177fe0533856f57d745d5000e98777110db6491
Julius B <Julius B>
2016-10-02 15:37:27 +0200

    embedded Unity

---
+++ unityfiles/Assets/generateTilesScript.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class generateTilesScript : MonoBehaviour
+{
+
+    public GameObject Wall;
+    public GameObject Ground;
+    // Use this for initialization
+    void Start()
+    {
+        int[][] array2D = new int[5][] { new int[10],new int[10],new int[10],new int[10], new int[10] };
+        array2D[1][5] = 1;
+        array2D[1][6] = 1;
+        array2D[1][7] = 1;
+        array2D[1][8] = 1;
+        array2D[3][5] = 1;
+        array2D[3][6] = 1;
+        array2D[3][7] = 1;
+        array2D[3][8] = 1;
+        generateTiles(array2D);
+    }
+    public void generateTiles(int[][] list)
+    {
+        int xCo = 0;
+        int yCo = 0;
+        foreach (int[] x in list)
+        {
+            foreach (int y in x)
+            {
+                Debug.Log(y);
+                Instantiate(getTile(y), new Vector3(xCo, 0, yCo), Quaternion.identity);
+                yCo = yCo + 1;
+            }
+            yCo = 0;
+            xCo = xCo + 1;
+        }
+    }
+    public void generateOverlay(int[][] list)
+    {
+        int xCo = 0;
+        int yCo = 0;
+        foreach (int[] x in list)
+        {
+            foreach (int y in x)
+            {
+                Debug.Log(y);
+                Instantiate(getOverlay(y), new Vector3(xCo, 1, yCo), Quaternion.identity);
+                yCo = yCo + 1;
+            }
+            yCo = 0;
+            xCo = xCo + 1;
+        }
+    }
+    public GameObject getTile(int type)
+    {
+        switch (type)
+        {
+            case 0:
+                return Ground;
+            case 1:
+                return Wall;
+        }
+        return null;
+    }
+    public GameObject getOverlay(int type)
+    {
+        return null;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
