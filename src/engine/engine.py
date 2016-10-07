c931f7c70df7ba73dac2fae37e56f965e4fe78d4
Juli <julisep@outlook.de>
2016-10-07 10:30:38 +0200

    Update engine.py

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -4,12 +4,35 @@ class engine():
     def __init__(self):
         self.outputFile ="pyUnity.json"
         self.open()
-    def render(self, tiles, overlay = None, rotation = 0):
+    def render(self, tiles, dictionary = None, rotation = 0):
         print("render")
+        mapTiles = tiles
+        overlayTiles = tiles
+        xCo = 0
+        yCo = 0
+        for _ in mapTiles:
+            for tile in _:
+                if( tile.begehbar is False ): # pseudocode
+                    mapTiles[xCo][yCo] = 1 # wall
+                else:
+                    mapTiles[xCo][yCo] = 0 # ground
+                yCo = yCo + 1      
+            xCo = xCo + 1
+        #prepareOverlay    
+        xCo = 0
+        yCo = 0
+        for _ in overlayTiles:
+            for tile in _:
+                if( tile.item == None ): # pseudocode
+                    overlayTiles[xCo][yCo] = 0
+                else:
+                    overlayTiles[xCo][yCo] = dictionary.get(tile.item) # pseudocode
+                yCo = yCo + 1      
+            xCo = xCo + 1
+        
         file = open(self.outputFile, "w")
-        x = json.dumps({"tiles": tiles, "overlay": overlay, "playerRoataion": rotation, "playerStats": None},indent=4)
+        x = json.dumps({"tiles": tiles, "overlay": overlayTiles, "playerRoataion": rotation, "playerStats": None},indent=4)
         file.write(x)
-        #TODO output data
         file.close()
     def close(self):
         file = open(self.outputFile, "w")
