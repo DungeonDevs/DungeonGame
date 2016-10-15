f1e5300275a7c76dbc18f4cdd306575627dff8c1
Julius B <Julius B>
2016-10-15 09:42:13 +0200

    resolved packages

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # DungeonGame
 Authors: Maximilian Lohmann, Tom Bloecker, Jannis Jorre, Nicolas von Luetzow
 
+to run the game run __init__.py
+
 Original attributes:
 - courage
 - intelligence
---
+++ src/logic/main/__init__.py
@@ -0,0 +1,6 @@
+from src.logic.main.Launcher import *
+from src.logic.main.Entity import *
+from src.logic.main.Game import *
+from src.logic.main.Item import *
+from src.logic.main.Map import *
+from src.logic.main.Tile import *
