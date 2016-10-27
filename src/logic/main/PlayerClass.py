4e891e79f1cfa3a581958d7a7b3a7a9cf04bfa2a
niconinja <Nicolas.vL2000@web.de>
2016-10-27 17:06:53 +0200

    forgot a 0

--- src/logic/main/PlayerClass.py
+++ src/logic/main/PlayerClass.py
@@ -1,6 +1,6 @@
 class Knight():
     def __init__(self, ID = 0, attack = 10, health = 30, healing = 3):
-        self.stats = [0,0,0]
+        self.stats = [0,0,0,0]
         self.stats[0] = ID
         self.stats[1] = attack
         self.stats[2] = health
