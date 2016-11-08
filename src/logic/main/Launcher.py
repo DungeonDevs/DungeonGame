f3d73d69aada24f6da88aece2f51a968346b0117
niconinja <Nicolas.vL2000@web.de>
2016-11-08 18:05:32 +0100

    fixed fighting, added classes

--- src/logic/main/Launcher.py
+++ src/logic/main/Launcher.py
@@ -1,10 +1,11 @@
 from src.logic.main.Game import Game
-from src.logic.main.PlayerClass import Knight, Healer, Adventurer, Thief
+from src.logic.main.PlayerClass import *
 import tkinter
 
 '''
 creates a new instance of the game
 '''
+#game = Game(AppleFanboy())
 class Launcher:
     def __init__(self):
         game = Game(Healer())
